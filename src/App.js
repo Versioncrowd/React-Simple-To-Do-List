cdeee31157a822637f88694e964fb7e3d270c57f
Sabine Gottfried <sabine.gottfried@gmail.com>
2018-03-07 14:25:43 +0100

    Fixed the input value issue. Now on submit input will be emptied.

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ class App extends Component {
     super();
     this.state = {
     inputValue: '',
-    todoes: []
+    todoes: [],
+    disable: true
     }
   }
   
   submitHandler = (e) => {
     e.preventDefault();
     this.setState((prevState) => {
-      return {todoes: [...prevState.todoes, this.state.inputValue]}
+      return {todoes: [...prevState.todoes, this.state.inputValue],
+        inputValue: '', disable: true}
       })
   }
 
   updateInputValue = (e) => {
     e.preventDefault();
-    this.setState({
-      inputValue: e.target.value
+      this.setState({
+      inputValue: e.target.value,
+      disable: false
     })
   }
 
@@ -41,8 +44,8 @@ class App extends Component {
         <div className="form-style">
         <h2>To Do List</h2>
           <form onSubmit={this.submitHandler}>
-            <input className="todoinput" type="text" placeholder="Add To Do Item" onChange={this.updateInputValue} />
-            <button type="submit">Submit</button>
+            <input className="todoinput" type="text" placeholder="Add To Do Item" onChange={this.updateInputValue} value={this.state.inputValue} />
+            <button type="submit" disabled={this.state.disable}>Submit</button>
           </form>
         </div>{this.state.todoes.map((todo, index) => {
          return <ToDoList todo={todo} click={() => this.clickHandler(index)} index={index}/>
